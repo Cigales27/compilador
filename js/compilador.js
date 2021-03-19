9b282eb41db9dee13f9c9a8a507c2658be119792
Cigales27 <72766195+Cigales27@users.noreply.github.com>
2021-03-19 01:31:44 -0600

    Cambios

--- js/compilador.js
+++ js/compilador.js
@@ -1,23 +1,31 @@
 document.addEventListener('DOMContentLoaded', function ()
 {
     const ejecutar = document.getElementById("ejecutar");
-    ejecutar.addEventListener("click", function (event)
+    ejecutar.onclick = function ()
     {
         let textoEntrada = document.getElementById("entrada").value;
         let textoConclusion = document.getElementById("mostrador");
         let texto = []
-
-
+        const letras = /[a-zA-Z]/
+        const numeros = /[0-9]/
+        const palabrasReservadas = ["for", "while", "do", "document", "appendChild", "addEventListener"]
 
         for (const caracter of textoEntrada)
         {
-            if (caracter != ('a'-'z'))
+            if (letras.test(caracter))
             {
-                console.log("ok")
+                const add = document.createElement('p')
+                textoConclusion.appendChild(add)
+                var newContent = document.createTextNode(`${caracter} es letra`);
+                add.appendChild(newContent)
+
+            }else
+            {
+                add.appendChild(`${caracter} no es letra`)
             }
             texto.push(caracter)
         }
 
-    },false)
+    }
 })
 
