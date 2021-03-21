138b6a9f51055be12b568432759dd73710f6f305
Cigales27 <72766195+Cigales27@users.noreply.github.com>
2021-03-21 01:43:44 -0600

    Palabras reservadas

--- js/compilador.js
+++ js/compilador.js
@@ -1,48 +1,45 @@
-document.addEventListener('DOMContentLoaded', function ()
-{
+document.addEventListener('DOMContentLoaded', function () {
     const ejecutar = document.getElementById("ejecutar");
-    ejecutar.onclick = function ()
-    {
+    ejecutar.onclick = function () {
         let textoEntrada = document.getElementById("entrada").value;
         let textoConclusion = document.getElementById("mostrador");
         let texto = []
         const letras = /[a-zA-Z]/
         const numeros = /[0-9]/
-        const palabrasReservadas = ["for","while", "do", "document", "appendChild", "addEventListener"]
+        const palabrasReservadas = ["for", "while", "do", "document", "appendChild", "addEventListener", "if"]
 
         var buscador = textoEntrada.split(" ");
-        if (buscador == palabrasReservadas[0])
+        for (var i = 0; i < palabrasReservadas.length; i++)
         {
-            console.log(palabrasReservadas);
-        }
-        for (const caracter of textoEntrada)
-        {
-            if (letras.test(caracter))
+            for (var t = 0; t < buscador.length; t++)
             {
-                const add = docume      addnt.createElement('p')
-          .style.color = `#FFFFFF`
-        for (const caracter of textoEntrada)
-        {
-            if (letras.test(caracter))
-            {
-                const add = document.createElement('p')
-                add.style.color = `#FFFFFF`
-                textoConclusion.appendChild(add)
-                var newContent = document.createTextNode(`${caracter} es letra`);
-                add.appendChild(newContent)
-
-            }else
-            {
-                const add = document.createElement('p')
-                add.style.color = `#FFFFFF`
-
-                textoConclusion.appendChild(add)
-                var newContent = document.createTextNode(`${caracter} no es letra`);
-                add.appendChild(newContent)
+                if (buscador[t] == palabrasReservadas[i]) {
+                    console.log(buscador[t]);
+                    const add = document.createElement('p')
+                    add.style.color = `#FFFFFF`
+                    textoConclusion.appendChild(add)
+                    var newContent = document.createTextNode(`${buscador[t]} es palabra reservada`);
+                    add.appendChild(newContent)
+                }
             }
-            texto.push(caracter)
+
         }
 
+        // for (const caracter of textoEntrada) {
+        //     if (letras.test(caracter)) {
+        //         const add = document.createElement('p')
+        //         add.style.color = `#FFFFFF`
+        //         textoConclusion.appendChild(add)
+        //         var newContent = document.createTextNode(`${caracter} es letra`);
+        //         add.appendChild(newContent)
+        //     } else {
+        //         const add = document.createElement('p')
+        //         add.style.color = `#FFFFFF`
+        //         textoConclusion.appendChild(add)
+        //         var newContent = document.createTextNode(`${caracter} no es letra`);
+        //         add.appendChild(newContent)
+        //     }
+        //     texto.push(caracter)
+        // }
     }
 })
-
