88d5a6eae3994fbffa21f1ff5be1cb91f52e3c2c
Cigales27 <72766195+Cigales27@users.noreply.github.com>
2021-03-24 20:09:44 -0600

    Cambios basicos

--- js/compilador.js
+++ js/compilador.js
@@ -6,27 +6,54 @@ document.addEventListener('DOMContentLoaded', function () {
         let texto = []
         const letras = /[a-zA-Z]/
         const numeros = /[0-9]/
-        const palabrasReservadas = ["const", "let", "var","for","while", "do", "document", "appendChild", "addEventListener", "if"]
+        const saltoLinea = "\n"
+        const palabrasReservadas = ["const", "let", "var", "for", "while", "do", "document", "appendChild", "addEventListener", "if"]
         const operadoresMatematicos = ["+", "-", "*", "/"]
         const operadoresLogicos = ["<", ">", "<=", ">=", "==", "!=", "&&", "||", "!"]
         const operadoresAsignacion = ["++", "--", "=", "*=", "/=", "+=", "-=", "%="]
 
-
+        textoEntrada = textoEntrada.replace(/\n/g, " ")
+        //if (textoEntrada.test(saltoLinea))
         var buscador = textoEntrada.split(" ");
-        for (var i = 0; i < palabrasReservadas.length; i++)
-        {
-            for (var t = 0; t < buscador.length; t++)
-            {
-                if (buscador[t] == palabrasReservadas[i]) {
-                    console.log(buscador[t]);
-                    const add = document.createElement('p')
-                    add.style.color = `#FFFFFF`
-                    textoConclusion.appendChild(add)
-                    var newContent = document.createTextNode(`${buscador[t]} es palabra reservada`);
-                    add.appendChild(newContent)
-                }
+
+        for (var i = 0; i < buscador.length; i++) {
+            switch (buscador[i].test) {
+                case (letras):
+                    for (var t = 0; t < palabrasReservadas.length; t++) {
+                        var final = (palabrasReservadas.length) - 1
+                        if (buscador[i] == palabrasReservadas[t]) {
+                            agregarEtiqueta(buscador[i], "palabra reservada")
+                            t = final
+                        } else {
+                            var final = (palabrasReservadas.length) - 1
+                            if (t == final) {
+                                agregarEtiqueta(buscador[i], "palabra")
+                                t = final
+                            }
+                        }
+                    }
+                    break
+
+                case (numeros):
+                    agregarEtiqueta(buscador[i], "numero")
+                    break
+
+                default:
+                    agregarEtiqueta(buscador[i], "error")
             }
 
+            // for (var t = 0; t < buscador.length; t++) {
+            //
+            // }
+
+        }
+
+        function agregarEtiqueta(numero, tipo) {
+            const add = document.createElement('p')
+            add.style.color = `#FFFFFF`
+            textoConclusion.appendChild(add)
+            var newContent = document.createTextNode(`${numero} es \t${tipo}`);
+            add.appendChild(newContent)
         }
 
         // for (const caracter of textoEntrada) {
