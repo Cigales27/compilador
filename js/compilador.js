5d77f2e645a732b0a2942c8d741e245c670589ba
Cigales27 <72766195+Cigales27@users.noreply.github.com>
2021-03-28 01:08:24 -0600

    comentarios

--- js/compilador.js
+++ js/compilador.js
@@ -1,19 +1,27 @@
+//las funciones de JavaScript empiezan a cargar cuanto el html termine
 document.addEventListener('DOMContentLoaded', function () {
-
+    //Se obtiene por id el boton
     const ejecutar = document.getElementById("ejecutar")
+    //Inicia el evento del click
     ejecutar.onclick = function () {
+
+        //Se obtiene la tabla para poder mostrarla cuando se le da click
         var mos = document.getElementById("tabla")
         mos.classList.remove("ocultar")
+
+        //Se obtienne los valores
         let textoEntrada = document.getElementById("entrada").value
         let textoConclusion = document.getElementById("mostrador")
         let tabla = document.getElementById("contenidoTabla")
-        let texto = []
+
+        //Declaracion de las expresiones regulares que swe podrian utilizar
         const letras = /[a-zA-Z0-9]/
         const numeros = /[0-9]/
         const signos = /[\{\}\[\]\(\)]/
         const operadores = /[\=\<\>\*\+\-]/g
-        const saltoLinea = /\n\t/g
         const forCiclo = /[for\;\;\+\+]/
+
+        //Verificacion de llaves y corchetes
         var llaves = []
         var corchetes = []
         const palabrasReservadas = ["const", "let", "var", "for", "while", "do", "document", "appendChild", "addEventListener", "if", "replace"]
@@ -21,6 +29,7 @@ document.addEventListener('DOMContentLoaded', function () {
         const operadoresLogicos = ["<", ">", "<=", ">=", "==", "!=", "&&", "||", "!"]
         const operadoresAsignacion = ["++", "--", "=", "*=", "/=", "+=", "-=", "%="]
 
+        //Eliminacion de saltos de linea y tabulaciones
         textoEntrada = textoEntrada.replace(/\n/g, " ")
         textoEntrada = textoEntrada.replace(/\t/g, " ")
 
@@ -34,8 +43,8 @@ document.addEventListener('DOMContentLoaded', function () {
 
                 case signos.test(buscador[i]):
                     agregarFila(buscador[i], "signo")
-                    if (buscador[i] == "{" ||buscador[i] == "}") verificarLlave(buscador[i])
-                    if (buscador[i] == "("||buscador[i] == ")") verificarParentesis(buscador[i])
+                    if (buscador[i] == "{" || buscador[i] == "}") verificarLlave(buscador[i])
+                    if (buscador[i] == "(" || buscador[i] == ")") verificarParentesis(buscador[i])
                     break
 
                 case letras.test(buscador[i]):
@@ -46,8 +55,7 @@ document.addEventListener('DOMContentLoaded', function () {
                             t = final
                         } else {
                             if (t == final) {
-                                if (operadores.test(buscador[i+1]))
-                                {
+                                if (operadores.test(buscador[i + 1])) {
                                     agregarEtiqueta(buscador[i], "sin asignacion", "yellow")
                                 }
                                 agregarFila(buscador[i], "variable ")
@@ -65,12 +73,14 @@ document.addEventListener('DOMContentLoaded', function () {
             }
         }
 
+
+        //Verificacion de llaves y corchetes
         function verificarLlave(llave) {
             if (llave == "{") {
                 llaves.push(llave)
             }
             if (llave == "}") {
-                if (llaves.length==0) {
+                if (llaves.length == 0) {
                     agregarEtiqueta("{}", "error", "red")
                 }
                 llaves.pop("}")
@@ -82,15 +92,15 @@ document.addEventListener('DOMContentLoaded', function () {
                 corchetes.push(llave)
             }
             if (llave == ")") {
-                if (corchetes.length==0) {
+                if (corchetes.length == 0) {
                     agregarEtiqueta("()", "error", "red")
                 }
                 corchetes.pop(llave)
             }
         }
 
-        function agregarFila(lexema, token)
-        {
+        //Agrega los datos de la tabla
+        function agregarFila(lexema, token) {
             const add = document.createElement("tr")
             tabla.appendChild(add)
             const contLexema = document.createElement("td")
@@ -104,7 +114,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
         }
-
+        //Funcion para agregar los warnings y errores
         function agregarEtiqueta(numero, tipo, color) {
             const add = document.createElement('p')
             add.style.color = `${color}`
@@ -113,6 +123,7 @@ document.addEventListener('DOMContentLoaded', function () {
             add.appendChild(newContent)
         }
 
+        //Verificacion de llaves y corchetes
         if (llaves.length != 0) {
             agregarEtiqueta("{", "error", "red")
         }
